513b14be75f4a06504f9d4da1f56709089c17c0e
Ewelina Bukowska <ewelina.bukowska@snow.dog>
2017-06-22 13:50:38 +0200

    Google Vision Api key

---
+++ app/src/main/java/pl/snowdog/privparksmartblocker/config/Secret.java
@@ -0,0 +1,8 @@
+package pl.snowdog.privparksmartblocker.config;
+
+public class Secret {
+    /**
+     * Add API KEY to GOOGLE VISION
+     */
+    public static final String GOOGLE_VISION_API_KEY = "";
+}
